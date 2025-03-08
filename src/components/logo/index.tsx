2143a4f5d04ca3d8f13cde52f4dc0bf6cf49b58a
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2025-03-07 23:24:29 -0300

    feat: add dashboard page and layout with navbar integration; include theme switch component

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -4,11 +4,11 @@ import { Calculator } from 'lucide-react'
 
 export const Logo = () => {
   return (
-    <a href="/" className="flex items-center justify-center gap-2">
-      <Calculator className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />
-      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-semibold leading-tight tracking-tighter text-transparent">
+    <a href="/" className="flex items-center justify-center gap-1">
+      <Calculator className="stroke h-8 w-8 stroke-amber-500 stroke-[1.5]" />
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-2xl font-semibold leading-tight tracking-tighter text-transparent">
         Saldo
-        <span className="bg-gradient-to-r from-orange-500 to-amber-400 bg-clip-text text-3xl font-semibold leading-tight tracking-tighter text-transparent">
+        <span className="bg-gradient-to-r from-orange-500 to-amber-400 bg-clip-text text-2xl font-semibold leading-tight tracking-tighter text-transparent">
           +
         </span>
       </p>
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,17 +1,22 @@
 'use client'
+import { useState } from 'react'
+
+import { UserButton } from '@clerk/nextjs'
+import { Menu } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-import { Logo } from '@/components'
+import { Logo, ThemeSwitch } from '@/components'
 import { cn } from '@/lib/utils'
 
-import { buttonVariants } from '../ui/button'
+import { Button, buttonVariants } from '../ui/button'
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from '../ui/sheet'
 
 const items = [
   {
     id: '1',
     label: 'Dashboard',
-    link: '/',
+    link: '/dashboard',
   },
   {
     id: '2',
@@ -26,7 +31,15 @@ const items = [
 ]
 
 export const Navbar = () => {
-  const NavBarItem = ({ label, link }: { label: string; link: string }) => {
+  const NavBarItem = ({
+    label,
+    link,
+    onClickCallback,
+  }: {
+    label: string
+    link: string
+    onClickCallback?: () => void
+  }) => {
     const pathname = usePathname()
     const isActive = pathname === link
 
@@ -41,9 +54,15 @@ export const Navbar = () => {
             'w-full justify-start text-lg text-muted-foreground hover:text-foreground',
             isActive && 'text-foreground'
           )}
+          onClick={() => {
+            onClickCallback && onClickCallback()
+          }}
         >
           {label}
         </Link>
+        {isActive && (
+          <div className="roudend-xl absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-x-1/2 bg-primary-foreground md:block" />
+        )}
       </div>
     )
   }
@@ -60,6 +79,52 @@ export const Navbar = () => {
               ))}
             </div>
           </div>
+          <div className="flex items-center gap-2">
+            <ThemeSwitch />
+            <UserButton afterSignOutUrl="/sign-in" />
+          </div>
+        </nav>
+      </div>
+    )
+  }
+
+  const MobileNavbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+      <div className="block border-separate bg-background md:hidden">
+        <nav className="container flex items-center justify-between px-4">
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
+            <SheetTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Menu />
+              </Button>
+            </SheetTrigger>
+            <SheetContent className="w-[350px] pr-10 sm:w-[540px]" side="left">
+              <SheetTitle className="flex items-start pl-3">
+                <Logo />
+              </SheetTitle>
+              <div className="flex flex-col gap-1 pt-4">
+                {items.map((item) => (
+                  <NavBarItem
+                    key={item.label}
+                    label={item.label}
+                    link={item.link}
+                    onClickCallback={() => setIsOpen((prev) => !prev)}
+                  />
+                ))}
+              </div>
+            </SheetContent>
+          </Sheet>
+
+          <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+            <Logo />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <ThemeSwitch />
+            <UserButton afterSignOutUrl="/sign-in" />
+          </div>
         </nav>
       </div>
     )
@@ -68,6 +133,7 @@ export const Navbar = () => {
   return (
     <>
       <DesktopNavbar />
+      <MobileNavbar />
     </>
   )
 }
