0c07c3520e5992366362c45d9de2fe85555b0e40
MarcusPOliveira <mp.mp.oliveira@gmail.com>
2025-04-20 23:37:36 -0300

    chore: enable redirects for the dashboard route in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // redirects: async () => {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/',
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
+  reactStrictMode: false,
+  redirects: async () => {
+    return [
+      {
+        source: '/dashboard',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
